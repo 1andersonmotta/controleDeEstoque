5ee5ea5266f3cef139971c13378fcb334ca9b68f
1andersonmotta <devandersonmotta@gmail.com>
2023-08-08 10:55:59 -0300

    feature create login

--- backoffice/index.js
+++ backoffice/index.js
@@ -1,3 +1,28 @@
-const f_username = document.querySelector("#f_username");
+const f_email = document.querySelector("#f_email");
 const f_senha = document.querySelector("#f_senha");
-const btn_login = document.querySelector("btn_login");
+const btn_login = document.querySelector("#btn_login");
+
+let serv = null;
+const endpoint_config = `../config.txt`;
+fetch(endpoint_config)
+    .then(res => res.json())
+    .then(res => {
+        sessionStorage.setItem("servidor_nodered", res.servidor_nodered);
+        sessionStorage.setItem("versao", res.versao);
+        serv = res.servidor_nodered;
+    })
+
+
+
+btn_login.addEventListener('click', (evt) => {
+    if (serv != null) {
+        const email = f_email.value;
+        const senha = f_senha.value;
+        const endpoint = `${serv}/login/${email}/${senha}`
+        fetch(endpoint)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+            })
+    }
+})
--- backoffice/main.js
+++ backoffice/main.js
@@ -2,13 +2,14 @@ const btn_menuPrincipal = document.querySelector("#btn_menuPrincipal")
 const menuPrincipal = document.querySelector("#menuPrincipal");
 const todosmenusprincipais = [...document.querySelectorAll(".btn_menuItem")]
 
-const endpoint_config = `../config.txt`;
-fetch(endpoint_config)
-    .then(res => res.json())
-    .then(res => {
-        sessionStorage.setItem("servidor_nodered", res.servidor_nodered);
-        sessionStorage.setItem("versao", res.versao);
-    })
+// const endpoint_config = `../config.txt`;
+// fetch(endpoint_config)
+//     .then(res => res.json())
+//     .then(res => {
+//         sessionStorage.setItem("servidor_nodered", res.servidor_nodered);
+//         sessionStorage.setItem("versao", res.versao);
+//         serv = res.servidor_nodered;
+//     })
 
 btn_menuPrincipal.addEventListener("click", (evt) => {
     menuPrincipal.classList.toggle("ocultar");
