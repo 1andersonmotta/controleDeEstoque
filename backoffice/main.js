93f2aff061e1321f6a4f8be8e0d6a5563cbeec7c
1andersonmotta <devandersonmotta@gmail.com>
2023-08-25 11:09:29 -0300

    feature valid token for access

--- backoffice/main.js
+++ backoffice/main.js
@@ -1,37 +1,57 @@
-if (sessionStorage.getItem("n_pessoa_pessoa") == "-1") {
-    window.location.href = "./index.html";
+const serv = sessionStorage.getItem("servidor_nodered");
+
+const verificarToken = () => {
+    const token = sessionStorage.getItem("s_token_token");
+    const endpoint = `${serv}/verificatoken/${token}`;
+    fetch(endpoint)
+        .then(res => res.json())
+        .then(res => {
+            if (res[0].retorno == 200) {
+                pagina()
+            } else {
+                alert("Token Inválido")
+                console.log("token inválido")
+            }
+        });
 }
+verificarToken()
 
-const btn_menuPrincipal = document.querySelector("#btn_menuPrincipal")
-const menuPrincipal = document.querySelector("#menuPrincipal");
-const todosmenusprincipais = [...document.querySelectorAll(".btn_menuItem")]
-const divid = document.querySelector("#id");
-const divnome = document.querySelector("#nome");
-const btnlogoff = document.querySelector("#btnlogoff");
+const pagina = () => {
+
+    if (sessionStorage.getItem("n_pessoa_pessoa") == "-1") {
+        window.location.href = "./index.html";
+    }
+
+    const btn_menuPrincipal = document.querySelector("#btn_menuPrincipal")
+    const menuPrincipal = document.querySelector("#menuPrincipal");
+    const todosmenusprincipais = [...document.querySelectorAll(".btn_menuItem")]
+    const divid = document.querySelector("#id");
+    const divnome = document.querySelector("#nome");
+    const btnlogoff = document.querySelector("#btnlogoff");
 
 
-btn_menuPrincipal.addEventListener("click", (evt) => {
-    menuPrincipal.classList.toggle("ocultar");
-});
+    btn_menuPrincipal.addEventListener("click", (evt) => {
+        menuPrincipal.classList.toggle("ocultar");
+    });
 
-todosmenusprincipais.forEach(e => {
-    e.addEventListener("click", (evt) => {
-        menuPrincipal.classList.add("ocultar");
+    todosmenusprincipais.forEach(e => {
+        e.addEventListener("click", (evt) => {
+            menuPrincipal.classList.add("ocultar");
+        })
     })
-})
 
-btnlogoff.addEventListener("click", (evt) => {
-    sessionStorage.setItem("n_pessoa_pessoa", "-1")
-    sessionStorage.setItem("s_nome_pessoa", "-1")
-    window.location.href = "./index.html"
-});
+    btnlogoff.addEventListener("click", (evt) => {
+        sessionStorage.setItem("n_pessoa_pessoa", "-1")
+        sessionStorage.setItem("s_nome_pessoa", "-1")
+        window.location.href = "./index.html"
+    });
 
-const n_pessoa_pessoa = sessionStorage.getItem("n_pessoa_pessoa");
-const s_nome_pessoa = sessionStorage.getItem("s_nome_pessoa");
-divid.innerHTML = `id: ${n_pessoa_pessoa}`;
-divnome.innerHTML = `nome:${s_nome_pessoa}`;
+    const n_pessoa_pessoa = sessionStorage.getItem("n_pessoa_pessoa");
+    const s_nome_pessoa = sessionStorage.getItem("s_nome_pessoa");
+    divid.innerHTML = `id: ${n_pessoa_pessoa}`;
+    divnome.innerHTML = `nome:${s_nome_pessoa}`;
 
-
+}
 //animação
 // gsap.set(".logo", {
 //     background: "yellow",
